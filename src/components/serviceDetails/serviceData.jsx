399f3aaaa444c92a1b82dcd9aaac6d1cea08efd4
Salutah <salutahmoha@gmail.com>
2025-01-25 13:37:29 +0300

    Building a service data file

--- src/components/serviceDetails/serviceData.jsx
+++ src/components/serviceDetails/serviceData.jsx
@@ -1,41 +1,41 @@
-// import customer from "../../assets/customer.jpg";
-// import delivery from "../../assets/delivery.jpg";
-// import event from "../../assets/event.jpg";
-// import menu from "../../assets/menu.jpg";
-// import offers from "../../assets/offers.jpg";
-// import online from "../../assets/online.jpg";
+import customer from "../../assets/customer.jpg";
+import delivery from "../../assets/delivery.jpg";
+import event from "../../assets/event.jpg";
+import menu from "../../assets/menu.jpg";
+import offers from "../../assets/offers.jpg";
+import online from "../../assets/online.jpg";
 
-// const serviceData = [
-//   {
-//     title: "Online Table Reservations",
-//     image: online,
-//     text: "At as in understood an remarkably solicitude. Mean them very seen she she. Use totally written the observe pressed justice. Instantly cordially far intention recommend estimable yet her his. Ladies stairs enough esteem add fat all enable. Needed its design number winter see. Oh be me sure wise sons no.",
-//   },
-//   {
-//     title: "Food Delivery and Takeout Services",
-//     image: delivery,
-//     text: "At as in understood an remarkably solicitude. Mean them very seen she she. Use totally written the observe pressed justice. Instantly cordially far intention recommend estimable yet her his. Ladies stairs enough esteem add fat all enable. Needed its design number winter see. Oh be me sure wise sons no.",
-//   },
-//   {
-//     title: "Special Offers and Promotions",
-//     image: offers,
-//     text: "At as in understood an remarkably solicitude. Mean them very seen she she. Use totally written the observe pressed justice. Instantly cordially far intention recommend estimable yet her his. Ladies stairs enough esteem add fat all enable. Needed its design number winter see. Oh be me sure wise sons no.",
-//   },
-//   {
-//     title: "Menu Display and Customization",
-//     image: menu,
-//     text: "At as in understood an remarkably solicitude. Mean them very seen she she. Use totally written the observe pressed justice. Instantly cordially far intention recommend estimable yet her his. Ladies stairs enough esteem add fat all enable. Needed its design number winter see. Oh be me sure wise sons no. ",
-//   },
-//   {
-//     title: "Event Hosting and Catering",
-//     image: event,
-//     text: "At as in understood an remarkably solicitude. Mean them very seen she she. Use totally written the observe pressed justice. Instantly cordially far intention recommend estimable yet her his. Ladies stairs enough esteem add fat all enable. Needed its design number winter see. Oh be me sure wise sons no.",
-//   },
-//   {
-//     title: "Contact and Customer Support",
-//     image: customer,
-//     text: "At as in understood an remarkably solicitude. Mean them very seen she she. Use totally written the observe pressed justice. Instantly cordially far intention recommend estimable yet her his. Ladies stairs enough esteem add fat all enable. Needed its design number winter see. Oh be me sure wise sons no. ",
-//   },
-// ];
+const serviceData = [
+  {
+    title: "Online Table Reservations",
+    image: online,
+    text: "At as in understood an remarkably solicitude. Mean them very seen she she. Use totally written the observe pressed justice. Instantly cordially far intention recommend estimable yet her his. Ladies stairs enough esteem add fat all enable. Needed its design number winter see. Oh be me sure wise sons no.",
+  },
+  {
+    title: "Food Delivery and Takeout Services",
+    image: delivery,
+    text: "At as in understood an remarkably solicitude. Mean them very seen she she. Use totally written the observe pressed justice. Instantly cordially far intention recommend estimable yet her his. Ladies stairs enough esteem add fat all enable. Needed its design number winter see. Oh be me sure wise sons no.",
+  },
+  {
+    title: "Special Offers and Promotions",
+    image: offers,
+    text: "At as in understood an remarkably solicitude. Mean them very seen she she. Use totally written the observe pressed justice. Instantly cordially far intention recommend estimable yet her his. Ladies stairs enough esteem add fat all enable. Needed its design number winter see. Oh be me sure wise sons no.",
+  },
+  {
+    title: "Menu Display and Customization",
+    image: menu,
+    text: "At as in understood an remarkably solicitude. Mean them very seen she she. Use totally written the observe pressed justice. Instantly cordially far intention recommend estimable yet her his. Ladies stairs enough esteem add fat all enable. Needed its design number winter see. Oh be me sure wise sons no. ",
+  },
+  {
+    title: "Event Hosting and Catering",
+    image: event,
+    text: "At as in understood an remarkably solicitude. Mean them very seen she she. Use totally written the observe pressed justice. Instantly cordially far intention recommend estimable yet her his. Ladies stairs enough esteem add fat all enable. Needed its design number winter see. Oh be me sure wise sons no.",
+  },
+  {
+    title: "Contact and Customer Support",
+    image: customer,
+    text: "At as in understood an remarkably solicitude. Mean them very seen she she. Use totally written the observe pressed justice. Instantly cordially far intention recommend estimable yet her his. Ladies stairs enough esteem add fat all enable. Needed its design number winter see. Oh be me sure wise sons no. ",
+  },
+];
 
-// export default serviceData;
+export default serviceData;
